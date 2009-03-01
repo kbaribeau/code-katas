9c7427f346fba55b5d4f64e1773212e6a8ed1951
Kevin Baribeau <kbaribeau@kevin-baribeaus-mac-pro.local>
2009-03-01 02:08:18 -0600

    refactor

--- python/bowling-calisthenics/bowling.py
+++ python/bowling-calisthenics/bowling.py
@@ -2,8 +2,10 @@ class Bowling:
     def calculate(self, input):
         result = 0
         for char in input:
-            if (char == 'X'):
-                result += 10
-                continue
-            result += int(char)
+            result += self.calculateSingleRoll(char)
         return result
+
+    def calculateSingleRoll(self, roll):
+        if (roll == 'X'):
+            return 10
+        return int(roll)
