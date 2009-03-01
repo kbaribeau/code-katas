ac518a5923ed849691140c6dd20ecb8ab7ae23cb
Kevin Baribeau <kbaribeau@kevin-baribeaus-mac-pro.local>
2009-03-01 10:58:51 -0600

    refactoring: introduce Game object

--- python/bowling-calisthenics/bowling.py
+++ python/bowling-calisthenics/bowling.py
@@ -1,21 +1,44 @@
 class Bowling:
     def calculate(self, input):
         result = 0
+        game = Game(input)
         for index, char in enumerate(input):
-            roll = Roll(char)
-            result += roll.score()
-            if (char == 'X' and len(input) != index + 3):
-                nextRoll = Roll(input[index+1])
-                twoRollsLater = Roll(input[index+2])
-                result += nextRoll.score()
-                result += twoRollsLater.score()
+            frame = Frame(index, game)
+            result += frame.score()
         return result
 
+class Game:
+    def __init__(self, input):
+        self.input = input
+
+    def getRoll(self, frameIndex):
+        return Roll(self.input[frameIndex])
+    def len(self):
+        return len(self.input)
+        
+class Frame:
+    def __init__(self, frameIndex, game):
+        self.frameIndex = frameIndex
+        self.game = game
+
+    def score(self):
+        roll = self.game.getRoll(self.frameIndex)
+        result = roll.score()
+
+        if (roll.isStrike() and self.game.len() != self.frameIndex + 3):
+            nextRoll = self.game.getRoll(self.frameIndex+1)
+            twoRollsLater = self.game.getRoll(self.frameIndex+2)
+            result += nextRoll.score()
+            result += twoRollsLater.score()
+
+        return result
 
 class Roll:
 
     def __init__(self, value):
         self.value = value
+    def isStrike(self):
+        return self.value == 'X'
 
     def score(self):
         if (self.value == 'X'):
