a14080c7b3629b0666408b923eda1983be93cda5
Kevin Baribeau <kbaribeau@kevin-baribeaus-mac-pro.local>
2009-04-11 21:32:19 -0600

    remove else keyword remove 1-dot-per-line violation

--- java/bowlingCalisthenics/src/com/kbaribeau/kata/bowling/scorer/ScorerFactory.java
+++ java/bowlingCalisthenics/src/com/kbaribeau/kata/bowling/scorer/ScorerFactory.java
@@ -1,15 +1,18 @@
 package com.kbaribeau.kata.bowling.scorer;
 
+import com.kbaribeau.kata.bowling.Roll;
 import com.kbaribeau.kata.bowling.RollIterator;
 
 public class ScorerFactory {
     public static Scorer create(RollIterator rollIterator) {
-        if (rollIterator.getRoll().isStrike()) {
+        Roll currentRoll = rollIterator.getRoll();
+        if (currentRoll.isStrike()) {
             return new StrikeScorer(rollIterator);
-        } else if (rollIterator.getRoll().isSpare()) {
-            return new SpareScorer(rollIterator);
-        } else {
-            return new DefaultScorer(rollIterator);
         }
+        if (currentRoll.isSpare()) {
+            return new SpareScorer(rollIterator);
+        }
+        return new DefaultScorer(rollIterator);
+
     }
 }
